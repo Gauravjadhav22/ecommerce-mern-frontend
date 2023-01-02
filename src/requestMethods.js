47ba841ae545a8449ab3938f41a58aa81b95af25
gaurav jadhav <gauravjadhav7512@gmail.com>
2023-01-02 12:03:36 +0530

    api changed

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://shopping-app-mern-backend.herokuapp.com/api/v1";
+// const BASE_URL = "https://shopping-app-mern-backend.herokuapp.com/api/v1";
+const BASE_URL = "https://shopping-app-backend-mxw2.onrender.com/api/v1/";
 // const BASE_URL = "http://localhost:5000/api/v1";
 
 var token =
